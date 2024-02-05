abd4c370f4991688638ffafe4323d8e2b85dd287
davrinat <davrinat@gmail.com>
2024-02-05 07:15:58 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,67 @@
+rootProject.name = "otusJava"
+include("L01-gradle")
+
+include("L02-gradle2")
+include("L02-gradle2-libApi")
+include("L02-gradle2-libApiUse")
+include("L02-logging")
+
+include("L03-qa")
+include("L04-generics")
+include("L05-collections")
+include("L06-annotations")
+
+include("L08-gc:demo")
+include("L08-gc:homework")
+
+include("L09-docker")
+include("L10-byteCodes")
+include("L11-java8")
+
+include("L12-solid")
+include("L13-creationalPatterns")
+include("L14-behavioralPatterns")
+include("L15-structuralPatterns:demo")
+include("L15-structuralPatterns:homework")
+
+include("L16-io:demo")
+include("L16-io:homework")
+
+include("L17-nio")
+
+include("L18-jdbc:demo")
+include("L18-jdbc:homework")
+
+include("L20-hibernate")
+include("L21-cache")
+
+include("L22-jpql:class-demo")
+include("L22-jpql:homework-template")
+
+include ("L23-noSQL:mongo-db-demo")
+include ("L23-noSQL:mongo-db-reactive-demo")
+include ("L23-noSQL:neo4j-demo")
+include ("L23-noSQL:redis-demo")
+include ("L23-noSQL:cassandra-demo")
+
+pluginManagement {
+    val jgitver: String by settings
+    val dependencyManagement: String by settings
+    val springframeworkBoot: String by settings
+    val johnrengelmanShadow: String by settings
+    val jib: String by settings
+    val protobufVer: String by settings
+    val sonarlint: String by settings
+    val spotless: String by settings
+
+    plugins {
+        id("fr.brouillard.oss.gradle.jgitver") version jgitver
+        id("io.spring.dependency-management") version dependencyManagement
+        id("org.springframework.boot") version springframeworkBoot
+        id("com.github.johnrengelman.shadow") version johnrengelmanShadow
+        id("com.google.cloud.tools.jib") version jib
+        id("com.google.protobuf") version protobufVer
+        id("name.remal.sonarlint") version sonarlint
+        id("com.diffplug.spotless") version spotless
+    }
+}
