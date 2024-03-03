ef9bdc4f4399976ee2f917e5b277572da67ecfa2
davrinat <davrinat@gmail.com>
2024-03-03 12:18:43 +0300

    init

--- L28-springDataJDBC/homework/build.gradle.kts
+++ L28-springDataJDBC/homework/build.gradle.kts
@@ -9,6 +9,16 @@ repositories {
 }
 
 dependencies {
+    implementation ("org.projectlombok:lombok")
+    annotationProcessor ("org.projectlombok:lombok")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+    implementation("org.postgresql:postgresql")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
